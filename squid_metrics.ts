8bb531d106538ed5f03c2415ada445dff07f5a3d
dircksen <filipeduf@gmail.com>
2023-06-23 16:07:18 -0300

    removing duplicate file

--- squid_metrics.ts
+++
@@ -1,7 +0,0 @@
-declare module 'squid-metrics'{
-  export function Configure(): void;
-
-  export function StartServer(port?: number): void;
-  export function GetExpressInstrumentationMiddleware(expressServer: any, customizedOptions: any): any;
-  export function GetHappiInstrumentationPlugin(customizedOptions: any): any;
-}
