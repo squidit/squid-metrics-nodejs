2b4415ee3b838a57bd3a318e4d99b11dea718fc8
Marcelo Morais <marcelo.fernandes@squidit.com.br>
2024-08-16 10:24:03 -0300

    adição de método para finalização de servidor de métricas e adição de tipagem

--- squid_metrics.d.ts
+++ squid_metrics.d.ts
@@ -1,7 +1,20 @@
-declare module 'squid-metrics'{
-  export function Configure(): void;
-
-  export function StartServer(port?: number): void;
-  export function GetExpressInstrumentationMiddleware(expressServer: any, customizedOptions: any): any;
-  export function GetHappiInstrumentationPlugin(customizedOptions: any): any;
+declare module "squid_metrics" {
+    export type ServerInstance = NonNullable<(Parameters<typeof createMiddleware>[0])>["app"];
+    export type PromsterOptions = NonNullable<(Parameters<typeof createMiddleware>[0])>["options"];
+    export function StartServer(port?: number): Promise<void>;
+    /**
+     * @param {ServerInstance} expressServer
+     * @param {PromsterOptions} customizedOptions
+     */
+    export function GetExpressInstrumentationMiddleware(expressServer: ServerInstance, customizedOptions: PromsterOptions): (request: import("express").Request, response: import("express").Response, next: import("express").NextFunction) => void;
+    /**
+     * @param {PromsterOptions} [customizedOptions]
+     */
+    export function GetHappiInstrumentationPlugin(customizedOptions?: PromsterOptions): import("@hapi/hapi").Plugin<unknown>;
+    /**
+     * @returns {Promise<void>}
+     */
+    export function CloseMetricsServer(): Promise<void>;
+    import { createMiddleware } from "@promster/express/dist/declarations/src";
 }
+//# sourceMappingURL=squid_metrics.d.ts.map
