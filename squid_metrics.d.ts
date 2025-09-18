0f2b0dc137e266337d1b4be18289b7033d53eb17
Marcelo Fernandes <marcelo.fernandes@wake.tech>
2025-09-18 13:57:45 -0300

    exposição de prometheus client

--- squid_metrics.d.ts
+++ squid_metrics.d.ts
@@ -15,6 +15,18 @@ declare module "squid_metrics" {
      * @returns {Promise<void>}
      */
     export function CloseMetricsServer(): Promise<void>;
+    /**
+     * Returns the Prometheus client (prom-client) for creating custom metrics
+     * @returns {typeof import('prom-client')}
+     */
+    export function GetPrometheusClient(): typeof import("prom-client");
+    /**
+     * Returns the default Prometheus registry used by @promster
+     * This is the same registry where HTTP metrics are registered
+     * @returns {import('prom-client').Registry}
+     */
+    export function GetPrometheusRegistry(): import("prom-client").Registry;
     import { createMiddleware } from "@promster/express/dist/declarations/src";
+    import { Prometheus } from "@promster/metrics/dist/declarations/src";
 }
 //# sourceMappingURL=squid_metrics.d.ts.map
